8bca686e8da82d2443613f7c44f9b272adc05ba4
Your Name <you@example.com>
2025-03-21 15:59:14 -0300

    new try

--- exercises.c
+++ exercises.c
@@ -129,7 +129,7 @@ int parentesisBalanceados(char *cadena) {
          if(top(P1) == NULL){
             return 0;
          }
-         pop(P1);
+         //pop(P1);
          char* comprobar = (char*)pop(P1);
          if((bl == ')' && comprobar != '(')  ||
             (bl == '}' && comprobar != '{' ) ||
